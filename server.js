13cb796977d63a0061d9bb1c3c76cf08f42a5168
Romain Lisowski <romain.lisowski@gmail.com>
2020-04-13 01:28:30 +0200

    Clean

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const socketIO = require('socket.io');
+const express = require("express");
+const socketIO = require("socket.io");
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const server = express().listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
 
