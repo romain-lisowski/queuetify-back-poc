81c51c96571366adce67487a5538c4c07e862bcd
Romain Lisowski <>
2020-04-17 13:05:57 +0200

    Add access-control-allow-origin

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(helmet);
 app.use(cors);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 // firebase
